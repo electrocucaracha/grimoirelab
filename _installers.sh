82d7d5ab4f7743ca464064768163390e373dfd61
Victor Morales <victor.morales@intel.com>
2019-07-08 13:35:33 -0700

    Separate latest and stable Docker images

---
+++ _installers.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2019
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o pipefail
+
+# _install_docker() - Download and install docker-engine
+function _install_docker {
+    local chameleonsocks_filename=chameleonsocks.sh
+
+    if command -v docker; then
+        return
+    fi
+    echo "Installing docker service..."
+    curl -fsSL https://get.docker.com/ | sh
+    sudo mkdir -p /etc/systemd/system/docker.service.d/
+    mkdir -p "$HOME/.docker/"
+    sudo usermod -aG docker "$USER"
+
+    if [ -n "${HTTP_PROXY:-}" ] || [ -n "${HTTPS_PROXY:-}" ] || [ -n "${NO_PROXY:-}" ]; then
+        config="{ \"proxies\": { \"default\": { "
+        if [ -n "${HTTP_PROXY:-}" ]; then
+            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf
+            echo "Environment=\"HTTP_PROXY=$HTTP_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/http-proxy.conf
+            config+="\"httpProxy\": \"$HTTP_PROXY\","
+        fi
+        if [ -n "${HTTPS_PROXY:-}" ]; then
+            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/https-proxy.conf
+            echo "Environment=\"HTTPS_PROXY=$HTTPS_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/https-proxy.conf
+            config+="\"httpsProxy\": \"$HTTPS_PROXY\","
+        fi
+        if [ -n "${NO_PROXY:-}" ]; then
+            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/no-proxy.conf
+            echo "Environment=\"NO_PROXY=$NO_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/no-proxy.conf
+            config+="\"noProxy\": \"$NO_PROXY\","
+        fi
+        echo "${config::-1} } } }" | tee "$HOME/.docker/config.json"
+        sudo systemctl daemon-reload
+        sudo systemctl restart docker
+    elif [ -n "${SOCKS_PROXY:-}" ]; then
+        wget "https://raw.githubusercontent.com/crops/chameleonsocks/master/$chameleonsocks_filename"
+        chmod 755 "$chameleonsocks_filename"
+        socks_tmp="${SOCKS_PROXY#*//}"
+        sudo ./$chameleonsocks_filename --uninstall
+        sudo PROXY="${socks_tmp%:*}" PORT="${socks_tmp#*:}" ./$chameleonsocks_filename --install
+        rm $chameleonsocks_filename
+    fi
+}
+
+# _install_jq() - Install a JSON processor
+function _install_jq {
+    if command -v jq; then
+        return
+    fi
+
+    echo "Installing JSON processor..."
+    # shellcheck disable=SC1091
+    source /etc/os-release || source /usr/lib/os-release
+    case ${ID,,} in
+        *suse)
+        INSTALLER_CMD="sudo -H -E zypper -q install -y --no-recommends"
+        sudo zypper -n ref
+        ;;
+
+        ubuntu|debian)
+        INSTALLER_CMD="sudo -H -E apt-get -y -q=3 install"
+        sudo apt-get update
+        ;;
+
+        rhel|centos|fedora)
+        PKG_MANAGER=$(command -v dnf || command -v yum)
+        INSTALLER_CMD="sudo -H -E ${PKG_MANAGER} -q -y install"
+        sudo "$PKG_MANAGER" updateinfo
+        ;;
+    esac
+    ${INSTALLER_CMD} jq
+}
+
+# install_docker_compose() - Installs docker compose python module
+function install_docker_compose {
+    _install_docker
+    if ! command -v pip; then
+        echo "Installing python package manager..."
+        curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
+    fi
+    if ! command -v docker-compose; then
+        echo "Installing docker-compose tool..."
+        sudo -E pip install docker-compose==1.24.0
+    fi
+}
--- postinstall.sh
+++ postinstall.sh
@@ -11,90 +11,7 @@
 set -o nounset
 set -o pipefail
 
-# _install_docker() - Download and install docker-engine
-function _install_docker {
-    local chameleonsocks_filename=chameleonsocks.sh
-
-    if command -v docker; then
-        return
-    fi
-    echo "Installing docker service..."
-    curl -fsSL https://get.docker.com/ | sh
-    sudo mkdir -p /etc/systemd/system/docker.service.d/
-    mkdir -p "$HOME/.docker/"
-    sudo usermod -aG docker "$USER"
-
-    if [ -n "${HTTP_PROXY:-}" ] || [ -n "${HTTPS_PROXY:-}" ] || [ -n "${NO_PROXY:-}" ]; then
-        config="{ \"proxies\": { \"default\": { "
-        if [ -n "${HTTP_PROXY:-}" ]; then
-            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf
-            echo "Environment=\"HTTP_PROXY=$HTTP_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/http-proxy.conf
-            config+="\"httpProxy\": \"$HTTP_PROXY\","
-        fi
-        if [ -n "${HTTPS_PROXY:-}" ]; then
-            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/https-proxy.conf
-            echo "Environment=\"HTTPS_PROXY=$HTTPS_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/https-proxy.conf
-            config+="\"httpsProxy\": \"$HTTPS_PROXY\","
-        fi
-        if [ -n "${NO_PROXY:-}" ]; then
-            echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/no-proxy.conf
-            echo "Environment=\"NO_PROXY=$NO_PROXY\"" | sudo tee --append /etc/systemd/system/docker.service.d/no-proxy.conf
-            config+="\"noProxy\": \"$NO_PROXY\","
-        fi
-        echo "${config::-1} } } }" | tee "$HOME/.docker/config.json"
-        sudo systemctl daemon-reload
-        sudo systemctl restart docker
-    elif [ -n "${SOCKS_PROXY:-}" ]; then
-        wget "https://raw.githubusercontent.com/crops/chameleonsocks/master/$chameleonsocks_filename"
-        chmod 755 "$chameleonsocks_filename"
-        socks_tmp="${SOCKS_PROXY#*//}"
-        sudo ./$chameleonsocks_filename --uninstall
-        sudo PROXY="${socks_tmp%:*}" PORT="${socks_tmp#*:}" ./$chameleonsocks_filename --install
-        rm $chameleonsocks_filename
-    fi
-}
-
-# _install_jq() - Install a JSON processor
-function _install_jq {
-    if command -v jq; then
-        return
-    fi
-
-    echo "Installing JSON processor..."
-    # shellcheck disable=SC1091
-    source /etc/os-release || source /usr/lib/os-release
-    case ${ID,,} in
-        *suse)
-        INSTALLER_CMD="sudo -H -E zypper -q install -y --no-recommends"
-        sudo zypper -n ref
-        ;;
-
-        ubuntu|debian)
-        INSTALLER_CMD="sudo -H -E apt-get -y -q=3 install"
-        sudo apt-get update
-        ;;
-
-        rhel|centos|fedora)
-        PKG_MANAGER=$(command -v dnf || command -v yum)
-        INSTALLER_CMD="sudo -H -E ${PKG_MANAGER} -q -y install"
-        sudo "$PKG_MANAGER" updateinfo
-        ;;
-    esac
-    ${INSTALLER_CMD} jq
-}
-
-# install_docker_compose() - Installs docker compose python module
-function install_docker_compose {
-    _install_docker
-    if ! command -v pip; then
-        echo "Installing python package manager..."
-        curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
-    fi
-    if ! command -v docker-compose; then
-        echo "Installing docker-compose tool..."
-        sudo -E pip install docker-compose==1.24.0
-    fi
-}
+source _installers.sh
 
 install_docker_compose
 sudo sysctl -w vm.max_map_count=262144
@@ -117,27 +34,13 @@ if [ ! -f conf/projects.json ]; then
     echo "${projects::-1} ] } }" | jq . | tee "conf/projects.json"
 fi
 
-# Clean up
-sudo docker-compose down --remove-orphans
-sudo rm -rf logs/*
+source cleanup.sh
 
+docker_compose_cmd="sudo docker-compose --file docker-compose.yml --file docker-compose.${GRIMOIRELAB_DEPLOY_TAG:-stable}.yml"
 # Deployment
 if [ "${GRIMOIRELAB_DEPLOY_MODE:-pull}" == "build" ]; then
-    declare -A services
-    services=(
-["arthurd"]="electrocucaracha/grimoirelab-kingarthur"
-["arthurw"]="electrocucaracha/grimoirelab-kingarthur"
-["bestiary"]="electrocucaracha/grimoirelab-bestiary"
-["mordred"]="electrocucaracha/grimoirelab-sirmordred"
-["sortinghat"]="electrocucaracha/grimoirelab-sortinghat"
-    )
-    for service in "${!services[@]}"; do
-        image=${services[$service]}
-        if ! docker images | grep "$image" ; then
-            sudo docker-compose build --no-cache "$service"
-        fi
-    done
+    ${docker_compose_cmd} build --no-cache
 else
-    sudo docker-compose pull
+    ${docker_compose_cmd} pull
 fi
-sudo docker-compose up --scale arthurw=10 --force-recreate --renew-anon-volumes --detach
+${docker_compose_cmd} up --scale arthurw=10 --force-recreate --renew-anon-volumes --detach
